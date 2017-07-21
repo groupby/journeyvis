01b4e3122ffc63c21eacce6e44b2505efb2d0db3
Haowei-Guo <haowei.guo@groupbyinc.com>
2017-07-20 23:31:42 -0400

    WIP: converter

--- data/converter.cc
+++ data/converter.cc
@@ -1,21 +1,87 @@
-#include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    cout << "Expect exactly one arguement." << endl;
-    return -1;
+class Event {
+public:
+  char type;
+  Event* search;
+  Event* view;
+  Event* order;
+  int number;
+  Event(char type): type(type), number(1), search(nullptr), view(nullptr), order(nullptr) {}
+  ~Event() {}
+
+  void increase() {
+    number++;
   }
 
-  fstream fs(argv[1]);
+  void print() {
+    cout << type << ": "<< number << endl;
+    if (search)
+      search->print();
+    if (view)
+      view->print();
+    if (order)
+      order->print();
+  }
+};
+
+class Wrapper {
+private:
+  Event* head;
+  Event* cursor;
+public:
+  Wrapper():head(nullptr), cursor(nullptr) {}
+
+  void setCursor() {
+    cursor = head;
+  }
+
+  void setCursor(char type) {
+    if (cursor == nullptr)
+      return;
+
+    switch(type) {
+      case 's': cursor = cursor->search; break;
+      case 'v': cursor = cursor->view; break;
+      case 'o': cursor = cursor->order; break;
+      default: break;
+    }
+  }
+
+  void insert(char type) {
+    if (head == nullptr) {
+      head = new Event(type);
+      cursor = head;
+    } else {
+      // TODO
+    }
+  }
+
+  void print() {
+    head->print();
+  }
+};
+
+Wrapper wrapper;
+
+int main() {
   string line;
 
-  while(getline(fs, line)) {
-    cout << line << endl;
+  while(getline(cin, line)) {
+    stringstream ss;
+    ss << line;
+    string event;
+    while (ss >> event) {
+      wrapper.insert(event[0]);
+    }
+    wrapper.setCursor();
   }
+
+  wrapper.print();
 
   return 0;
 }
